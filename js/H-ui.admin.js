8f6766d6f83fa43d1665fa88a55139d1a67816bd
袁翰林 <yuanhanlin@outlook.com>
2016-09-22 23:01:29 +0800

    增加登录状态验证
    利用sessionStorage中的isLogin来判断是否已经登陆。

--- js/H-ui.admin.js
+++ js/H-ui.admin.js
@@ -187,6 +187,36 @@ function Tab_or_layer()
       		layer.full(index);	
       	}
 	}
+/*在访问这个页面的时候需要验证是否已经登陆*/
+$(function()
+{
+	console.log('login check');
+	if (typeof(Storage)!=='undefined')
+	{
+		console.log(sessionStorage.isLogin)
+		if ((!sessionStorage.isLogin)||(sessionStorage.isLogin == 'false'))
+		{
+			console.log('tz');
+			location.href = 'login.html';
+			/*var userName = sessionStorage.userName;
+			$('#nav_user_btn').html('<i class="Hui-iconfont">\&\#xe62c;</i> 
+			'+userName+'<i class="Hui-iconfont">\&\#xe6d5;</i>');*/
+			/*layer.open({
+				type: 2,
+				area: ['100%', '100%'],
+				fix: true, //不固定
+				maxmin: false,
+				shade:0.4,
+				title: '登录',
+				content: 'login.html',
+				closeBtn:0,
+			});*/
+		} else 
+		{
+			alert('welcome '+sessionStorage.userName+" !");
+		}
+	}
+});
 $(function(){
 	getskincookie();
 	//layer.config({extend: 'extend/layer.ext.js'});
