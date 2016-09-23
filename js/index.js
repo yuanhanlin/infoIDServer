8aa0be0b1f10ed6e99c5ee0892a5c78426fceef1
袁翰林 <yuanhanlin@outlook.com>
2016-09-23 14:19:14 +0800

    9月23日上午修改
    1.做任务页面是否通过改成抵押状态
    2.查询页面申请之后添加“做任务”和“查询“以及”开始结束时间“

---
+++ js/index.js
@@ -0,0 +1,30 @@
+/*在访问这个页面的时候需要验证是否已经登陆*/
+$(function()
+{
+	console.log('login check');
+	if (typeof(Storage)!=='undefined')
+	{
+		console.log(sessionStorage.isLogin)
+		if ((!sessionStorage.isLogin)||(sessionStorage.isLogin == 'false'))
+		{
+			console.log('tz');
+			location.href = 'login.html';
+			/*var userName = sessionStorage.userName;
+			$('#nav_user_btn').html('<i class="Hui-iconfont">\&\#xe62c;</i> 
+			'+userName+'<i class="Hui-iconfont">\&\#xe6d5;</i>');*/
+			/*layer.open({
+				type: 2,
+				area: ['100%', '100%'],
+				fix: true, //不固定
+				maxmin: false,
+				shade:0.4,
+				title: '登录',
+				content: 'login.html',
+				closeBtn:0,
+			});*/
+		} else 
+		{
+			alert('welcome '+sessionStorage.userName+" !");
+		}
+	}
+});
